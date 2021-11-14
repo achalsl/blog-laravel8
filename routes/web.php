08f6b020ad3e2290bd113c25a844482bb108d036
Achal Lalwani <achallalwani@Achals-MacBook-Air.local>
2021-11-15 01:41:28 +0530

    added post and category relationships, added clockwork for debugging

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
@@ -17,7 +18,7 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
+    $posts = Post::with('category')->get();
     return view('posts', [
         'posts' => $posts
     ]);
@@ -27,5 +28,12 @@ Route::get('posts/{post}', function (Post $post) {
 // Find a post by its slug and pass it to a view called post
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
